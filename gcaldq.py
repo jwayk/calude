234abeb0383ee639b5cbdb4fe5ddccbadcee7b4c
jwayk <jwayk15@gmail.com>
2023-12-21 09:10:45 -0500

    add basic multithreading

--- gcaldq.py
+++ gcaldq.py
@@ -1,16 +1,27 @@
+from concurrent.futures import ThreadPoolExecutor
+
 from schedule_parser import ScheduleParser, Run
 from google_interface import CalendarInterface
 import settings
 
 
-if __name__ == "__main__":
+def parse_schedule():
     print("Parsing GDQ schedule...")
     parser = ScheduleParser()
     parsed_runs = parser.parse()
     print(f"Parsed {len(parsed_runs)} runs")
+    return parsed_runs
 
+def initialize_calendar():
     print("Initializing calendar interface...")
-    calendar = CalendarInterface(settings.calendar_id, settings.clear_calendar)
+    return CalendarInterface(settings.calendar_id, settings.clear_calendar)
+
+
+if __name__ == "__main__":
+
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        future = executor.submit(lambda function: function(), parse_schedule, initialize_calendar)
+        parsed_runs, calendar = future.result()
 
     existing_events = calendar.get_all_events()
     outdated_events = calendar.find_outdated_events(parsed_runs)
